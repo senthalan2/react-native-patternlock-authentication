cc7e8c5119d374559df9387102a011516f4239e4
senthalan2 <senthalan2@gmail.com>
2022-01-17 14:16:00 +0530

    Normal Pattern Lock Changes

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,24 @@
 import * as React from 'react';
 
 import { Dimensions, StyleSheet, View } from 'react-native';
-import { PatternLock } from 'react-native-patternlock-authentication';
+import {
+  PatternLock,
+  NormalPatternLock,
+} from 'react-native-patternlock-authentication';
+
+const { width, height } = Dimensions.get('window');
+const PATTERN_CONTAINER_HEIGHT = height / 2;
+const PATTERN_CONTAINER_WIDTH = width;
+const PATTERN_DIMENSION = 3;
+const CORRECT_UNLOCK_PATTERN = [
+  { x: 0, y: 0 },
+  { x: 1, y: 0 },
+  { x: 2, y: 0 },
+  { x: 1, y: 1 },
+  { x: 0, y: 2 },
+  { x: 1, y: 2 },
+  { x: 2, y: 2 },
+];
 
 export default function App() {
   const onPatternMatch = () => {
@@ -12,13 +29,40 @@ export default function App() {
     console.log('onWrongPattern');
   };
 
+  const onPatternMatchAfterDelay = () => {
+    console.log('onPatternMatchAfterDelay');
+  };
+
+  const onWrongPatternAfterDelay = () => {
+    console.log('onWrongPatternAfterDelay');
+  };
+
   return (
     <View style={styles.container}>
-      <PatternLock
+      {/* <PatternLock
         onPatternMatch={onPatternMatch}
         onWrongPattern={onWrongPattern}
         isChangePattern={false}
         processName="set_pattern"
+      /> */}
+      <NormalPatternLock
+        containerDimension={PATTERN_DIMENSION}
+        containerWidth={PATTERN_CONTAINER_WIDTH}
+        containerHeight={PATTERN_CONTAINER_HEIGHT}
+        correctPattern={CORRECT_UNLOCK_PATTERN}
+        dotsAndLineColor="blue"
+        wrongPatternDelayTime={1000}
+        correctPatternDelayTime={1000}
+        defaultDotRadius={10}
+        snapDotRadius={15}
+        snapDuration={100}
+        lineStrokeWidth={5}
+        wrongPatternColor="red"
+        matchedPatternColor="green"
+        onPatternMatch={onPatternMatch}
+        onWrongPatternAfterDelay={onWrongPatternAfterDelay}
+        onPatternMatchAfterDelay={onPatternMatchAfterDelay}
+        onWrongPattern={onWrongPattern}
       />
     </View>
   );
