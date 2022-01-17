adbf6d775e237c73f22789d08678cf6d10251453
senthalan2 <senthalan2@gmail.com>
2022-01-17 08:18:16 +0530

    Pull Code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import PatternLock from './PatternLockAuthentication/PatternLockAuthentication';
 import PatternProcess from './PatternLockAuthentication/PatternProcess';
 module.exports = {
   PatternLock,
-  PatternProcess,
 };
+
+export { PatternProcess };
